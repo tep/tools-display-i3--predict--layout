38dba667e6dfd9cfaa4e4327c3b0fcde32414d07
Tim Peoples <tim@toolman.org>
2019-06-13 12:33:25 -0700

    Add GPL

---
+++ go.mod
@@ -0,0 +1,10 @@
+module toolman.org/tools/display/i3-predict-layout
+
+go 1.12
+
+require (
+	github.com/spf13/pflag v1.0.3
+	go.i3wm.org/i3 v0.0.0-20190604153705-24af49cb29e4
+	toolman.org/base/log/v2 v2.1.0
+	toolman.org/base/toolman/v2 v2.1.2
+)
--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+// Copyright © 2018 Timothy E. Peoples <eng@toolman.org>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -9,8 +24,8 @@ import (
 
 	"go.i3wm.org/i3"
 
-	"toolman.org/base/log"
-	"toolman.org/base/toolman"
+	"toolman.org/base/log/v2"
+	"toolman.org/base/toolman/v2"
 )
 
 var (
